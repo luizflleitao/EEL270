70d0d3d37f6c812e9538ec1974df9a740b5ad724
luizflleitao <luizflleitao@users.noreply.github.com>
2019-04-19 02:55:59 -0300

    Add files via upload

---
+++ 2019-1/Extras/template.c
@@ -0,0 +1,15 @@
+/*
+ * Universidade Federal do Rio de Janeiro
+ * Escola Politecnica
+ * Departamento de Eletronica e de Computacao
+ * EEL270 - Computacao II - Turma 2019/1
+ * Prof. Marcelo Luiz Drumond Lanza
+ * Autor: Luiz Fernando Loureiro Leitao
+ *
+ * $Author$
+ * $Date$
+ * $Log$
+ */
+
+
+/* $RCSfile$ */
